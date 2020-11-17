1f2ec12d014231fa53a1b266ba41fe83f3720a70
Antti <anttiparhiala@hotmail.com>
2020-11-17 04:30:50 +0200

    RectF added

---
+++ Engine/RectF.cpp
@@ -0,0 +1,25 @@
+#include "RectF.h"
+
+RectF::RectF(float left_in, float top_in, float right_in, float bottom_in)
+	:
+	left(left_in),
+	top(top_in),
+	right(right_in),
+	bottom(bottom_in)
+{}
+
+RectF::RectF(const Vec2& topleft, const Vec2& bottomright)
+{
+	RectF(topleft.x, bottomright.x, topleft.y, bottomright.y);
+}
+
+RectF::RectF(const Vec2& topleft, float width, float height)
+{
+	RectF(topleft, topleft + Vec2 (width, height));
+}
+
+bool RectF::Overlap(const RectF& other) const
+{
+	return left < other.right && right > other.left &&
+		   top < other.bottom && bottom > other.top;
+}
---
+++ Engine/RectF.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Vec2.h"
+
+
+class RectF
+{
+public:
+	RectF() = default;
+	RectF(float left_in, float top_in, float right_in, float bottom_in);
+	RectF(const Vec2& topleft, const Vec2& bottomright);
+	RectF(const Vec2& topleft, float width, float height);
+	bool Overlap(const RectF& other) const;
+
+private:
+	float left;
+	float right;
+	float top;
+	float bottom;
+};
